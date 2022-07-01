5ef08678fdd2a441795655711a3fb74fc1a6e68b
mateasportfolio <104080590+mateasportfolio@users.noreply.github.com>
2022-07-01 10:26:41 +0200

    set SavingsForm into Code

--- src/components/SavingsForm/SavingsForm.jsx
+++ src/components/SavingsForm/SavingsForm.jsx
@@ -1,18 +1,24 @@
 import React, { useState } from "react";
 import "./style.css";
-import { FormControl, FormLabel, Input } from "@chakra-ui/react";
+import { FormControl, FormLabel } from "@chakra-ui/react";
+import { Input } from "@chakra-ui/react";
 
 export const SavingsForm = () => {
+  const [startingBalance, setstartingBalance] = useState(3000);
+  const [monthlyInstallment, setMonthlyInstallment] = useState(300);
   return (
     <div className="savings-form">
       <FormControl>
         <FormLabel>Starting Balance:</FormLabel>
         <Input
-          name="Starting Balance"
+          name="starting-balance"
           size="sm"
-          value={startingBalance(e.target.value)}
+          value={startingBalance}
+          on
+          onChange={(e) => setstartingBalance(e.target.value)}
         />
       </FormControl>
     </div>
   );
 };
+export default SavingsForm;
