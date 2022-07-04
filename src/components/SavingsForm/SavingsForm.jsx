fdf38d07a6f123417aecde90a6de02850ddb7fec
mateasportfolio <104080590+mateasportfolio@users.noreply.github.com>
2022-07-04 17:49:21 +0200

    add monthly income

--- src/components/SavingsForm/SavingsForm.jsx
+++ src/components/SavingsForm/SavingsForm.jsx
@@ -5,7 +5,9 @@ import { Input } from "@chakra-ui/react";
 
 export const SavingsForm = () => {
   const [startingBalance, setstartingBalance] = useState(3000);
-  const [monthlyInstallment, setMonthlyInstallment] = useState(300);
+  const [avgMonthlyIncome, setAvgMonthlyIncome] = useState(1000);
+  const [monthlyExpenses, setMonthlyExpenses] = useState(1500);
+
   return (
     <div className="savings-form">
       <FormControl>
@@ -16,6 +18,29 @@ export const SavingsForm = () => {
           value={startingBalance}
           on
           onChange={(e) => setstartingBalance(e.target.value)}
+          type="number"
+        />
+      </FormControl>
+      <FormControl>
+        <FormLabel>Average Monthly Income (after taxes):</FormLabel>
+        <Input
+          name="avg-monthly-income"
+          size="sm"
+          value={avgMonthlyIncome}
+          on
+          onChange={(e) => setAvgMonthlyIncome(e.target.value)}
+          type="number"
+        />
+      </FormControl>
+      <FormControl>
+        <FormLabel>Average Monthly Expenses:</FormLabel>
+        <Input
+          name="monthly-expenses"
+          size="sm"
+          value={monthlyExpenses}
+          on
+          onChange={(e) => setMonthlyExpenses(e.target.value)}
+          type="number"
         />
       </FormControl>
     </div>
