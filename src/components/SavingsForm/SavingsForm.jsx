08ed13547a9ec89971a2f50c5767c343ac6eca25
mateasportfolio <104080590+mateasportfolio@users.noreply.github.com>
2022-07-06 14:08:41 +0200

    add react calender in savingsform

--- src/components/SavingsForm/SavingsForm.jsx
+++ src/components/SavingsForm/SavingsForm.jsx
@@ -2,11 +2,14 @@ import React, { useState } from "react";
 import "./style.css";
 import { FormControl, FormLabel } from "@chakra-ui/react";
 import { Input } from "@chakra-ui/react";
+import Calendar from "react-calendar";
 
 export const SavingsForm = () => {
   const [startingBalance, setstartingBalance] = useState(3000);
   const [avgMonthlyIncome, setAvgMonthlyIncome] = useState(1000);
   const [monthlyExpenses, setMonthlyExpenses] = useState(1500);
+  const [financialOutlook, setfinancialOutlook] = useState(300);
+  const [value, onChange] = useState(new Date());
 
   return (
     <div className="savings-form">
@@ -41,6 +44,22 @@ export const SavingsForm = () => {
           on
           onChange={(e) => setMonthlyExpenses(e.target.value)}
           type="number"
+        />
+      </FormControl>
+      <FormControl>
+        <FormLabel>Projected financial outlook on :</FormLabel>
+
+        <Input
+          size="sm"
+          name="financial-outlook"
+          value={financialOutlook}
+          on
+          onChange={(e) => setfinancialOutlook(e.target.value)}
+          type="month"
+          id="endDate"
+          min="2016-06"
+          minDetail="century"
+          class="sc-pFZIQ gbgfMs"
         />
       </FormControl>
     </div>
