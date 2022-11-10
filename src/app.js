dffd9f813f1453e0195142dbae76328869012fed
mateasportfolio <104080590+mateasportfolio@users.noreply.github.com>
2022-11-10 18:27:55 +0100

    trying out use media querys and changing them

--- src/app.js
+++ src/app.js
@@ -3,14 +3,15 @@ import Navigation from "./components/Navigation/Navigation";
 import Header from "./components/Header/Header";
 import { SavingsForm } from "./components/SavingsForm/SavingsForm";
 import DebtForm from "./components/DebtForm/DebtForm";
-import { Box, Stack, Text } from "@chakra-ui/react";
+import { Box, Stack } from "@chakra-ui/react";
 import { useMediaQuery } from "@chakra-ui/media-query";
 
 function App() {
   const [component, setComponent] = useState("SAVINGS");
   // const [date, setDate] = useState(new Date());
   // const [isSmallScreen] = useMediaQuery("(max-width: 768px)");
-  const [isMobile] = useMediaQuery("(max-width: 768px)");
+  // const [isMobile] = useMediaQuery("(max-width: 768px)");
+  const [isSmallScreen] = useMediaQuery("(min-width: 768px)");
 
   const renderForm = () => {
     switch (component) {
@@ -26,18 +27,15 @@ function App() {
   return (
     <>
       <Stack>
-        <Text>
-          {isMobile ? "This is a mobile device" : "This is a desktop device"}
-        </Text>
+        {isSmallScreen ? "larger than 768px" : "smaller than 786px"}
       </Stack>
       <Box
-        // maxW="768px"
+        minW={isSmallScreen}
         minH={"calc(100vh)"}
         pb={40}
         mt={{ base: 4, md: 0 }}
         ml={{ md: 6 }}
         display={{ md: "flex" }}
-        // width={[1, 1 / 2, 1 / 4]}
         flexDirection="column"
         alignItems="center"
         backgroundColor={"black"}
